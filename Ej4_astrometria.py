511335319280cf8491e2bb590c38dff55638c633
SergioSE98 <SergioSE98@users.noreply.github.com>
2021-02-10 22:41:41 +0000

    Terminado ejercicio 4
    A falta de algún retoque estético (revisar si debo poner más claras las leyendas o títulos), el ejercicio 4 ya funciona correctamente.

--- Ej4_astrometria.py
+++ Ej4_astrometria.py
@@ -54,12 +54,15 @@ matches_gaia = smatch.match(ra1, dec1, rad, ra3, dec3, nside=nside, maxmatch=max
 
 
 #Imprimo las tablas con los valores de ascención recta y declinación que han hecho match:
+    
+"""
+#Quitando estre entrecomillado se imprimen los matches (redondea el coseno de la distancia angular a uno, por eso lo calculo usando un loop)
 
 print("\n Matches SHARKS-2MASS: ")
 print(matches_2mass)
 print("\n Matches SHARKS-GAIA: ")
 print(matches_gaia)
-
+"""
 
 #Puedo adicionalmente imprimir las columnas por separado para ver más claro qué valores de ascención recta y declinación han hecho match entre los catálogos.
 #(Esto último se puede comentar para sacar solo las tablas completas y no generar las columnas por separado.)
@@ -72,6 +75,9 @@ dec1matched_2mass = dec1[ matches_2mass['i1'] ]
 ra2matched_2mass  = ra2[ matches_2mass['i2'] ]
 dec2matched_2mass = dec2[ matches_2mass['i2'] ]
 
+"""
+#Quitando este entrecomillado se imprimen los valores de RA y DEC que hacen match.
+
 print("\nSHARKS RA matched with 2MASS")
 print(ra1matched_2mass)
 print("\nSHARKS DEC matched with 2MASS")
@@ -80,7 +86,7 @@ print("\n2MASS RA matched with SHARKS")
 print(ra2matched_2mass)
 print("\n2MASS DEC matched with SHARKS")
 print(dec2matched_2mass)
-
+"""
 
 #Columnas de RA y DEC que hacen match entre SHARKS y GAIA:
 
@@ -88,6 +94,9 @@ ra1matched_gaia  = ra1[ matches_gaia['i1'] ]
 dec1matched_gaia = dec1[ matches_gaia['i1'] ]
 ra3matched_gaia  = ra3[ matches_gaia['i2'] ]
 dec3matched_gaia = dec3[ matches_gaia['i2'] ]
+
+"""
+#Quitando este entrecomillado se imprimen los valores de RA y DEC que hacen match.
 
 print("\nSHARKS RA matched with GAIA")
 print(ra1matched_gaia)
@@ -97,5 +106,86 @@ print("\nGAIA RA matched with SHARKS")
 print(ra3matched_gaia)
 print("\nGAIA DEC matched with SHARKS")
 print(dec3matched_gaia)
+"""
+
+#Utilizo loops para calcular las diferencias entre los valores de RA y DEC que hacen matching, así comola distancia angular entre los puntos (gamma)
+
+#SHARKS-2MASS.
+
+#Diferencia entre valores de RA de SHARKS y 2MASS que hacen match
+dif_ra_2mass=[]
+for r1,r2 in zip(ra1matched_2mass,ra2matched_2mass):
+    #print(r1,r2)
+    dif_ra_2mass.append((r1-r2)*3600)  #multi por 3600 para pasar a grados
+    
+#print(dif_ra_2mass)
+#print(np.mean(dif_ra_2mass))
+#print(np.std(dif_ra_2mass))
+
+#Diferencia entre valores de DEC de SHARKS y 2MASS que hacen match
+dif_dec_2mass=[]
+for d1,d2 in zip(dec1matched_2mass,dec2matched_2mass):
+    #print(r1,r2)
+    dif_dec_2mass.append((d1-d2)*3600)  
+    
+#print(dif_dec_2mass)
+#print(np.mean(dif_dec_2mass))
+#print(np.std(dif_dec_2mass))
+
+#Distancia angular entre puntos que hacen match entre SHARKS y 2MASS
+gamma_2mass=[]
+for r1,r2,d1,d2 in zip(ra1matched_2mass, ra2matched_2mass, dec1matched_2mass, dec2matched_2mass):
+    gamma_2mass.append(np.arccos(np.cos(90-d1)*np.cos(90-d2)+np.sin(90-d1)*np.sin(90-d2)*np.cos(r1-r2))*3600)  #Esta formula en bibliografía
+    
+print(gamma_2mass)
+print(np.mean(gamma_2mass))
+print(np.std(gamma_2mass))
+
+image1 = plt.figure("Photometry histogramas SHARKS-2MASS")
+plt.title("SHARKS-2MASS matched point sources histogram")
+seaborn.distplot(dif_ra_2mass,bins=60, label= "RA diferences between matched point sources")
+seaborn.distplot(dif_dec_2mass,bins=60, label= "DEC diferences between matched point sources")
+seaborn.distplot(gamma_2mass,bins=60, label= "Angular distance between matched point sources") 
+plt.legend()
+plt.show()
 
 
+#Procedo ahora exactamente igual a como hice con SHARKS-2MASS para obtener los valores de distancia angular entre matches.
+
+#SHARKS-GAIA
+
+dif_ra_gaia=[]
+for r1,r3 in zip(ra1matched_gaia,ra3matched_gaia):
+    #print(r1,r2)
+    dif_ra_gaia.append((r1-r3)*3600)
+#print(dif_ra_2mass)
+#print(np.mean(dif_ra_2mass))
+#print(np.std(dif_ra_2mass))
+
+dif_dec_gaia=[]
+for d1,d3 in zip(dec1matched_gaia,dec3matched_gaia):
+    #print(r1,r2)
+    dif_dec_gaia.append((d1-d3)*3600)
+#print(dif_dec_2mass)
+#print(np.mean(dif_dec_2mass))
+#print(np.std(dif_dec_2mass))
+
+gamma_gaia=[]
+
+for r1,r3,d1,d3 in zip(ra1matched_gaia, ra3matched_gaia, dec1matched_gaia, dec3matched_gaia):
+    gamma_gaia.append(np.arccos(np.cos(90-d1)*np.cos(90-d3)+np.sin(90-d1)*np.sin(90-d3)*np.cos(r1-r3))*3600)
+    
+print(gamma_gaia)
+print(np.mean(gamma_gaia))
+print(np.std(gamma_gaia))
+
+image2 = plt.figure("Photometry histogramas SHARKS-GAIA")
+plt.title("SHARKS-GAIA matched point sources histogram")
+seaborn.distplot(dif_ra_gaia,bins=60, label= "RA diferences between matched point sources")
+seaborn.distplot(dif_dec_gaia,bins=60, label= "DEC diferences between matched point sources")
+seaborn.distplot(gamma_gaia,bins=60, label= "Angular distance between matched point sources") 
+plt.legend()
+plt.show()
+
+image1.savefig('Photometry histograms SHARKS-2MASS.png')
+image2.savefig('Photometry histograms SHARKS-GAIA.png')   
