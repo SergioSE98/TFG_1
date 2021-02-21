16dd5efb51b705c256af20064dd02f53c5f8740f
SergioSE98 <SergioSE98@users.noreply.github.com>
2021-02-21 22:07:17 +0000

    Añadido ej 5
    Añadido ej 5 para fotometría, (no funciona por ahora)

---
+++ Ej5_fotometria.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 21 19:59:06 2021
+
+@author: Sergio
+"""
+
+import numpy as np
+
+import astropy as astropy 
+
+import pandas as pandas
+
+import seaborn as seaborn
+
+import matplotlib.pyplot as plt
+
+from astropy.table import Table    #Ojo importante aquí importar "Table" con mayuscula
+
+import smatch
+
+#Empiezo haciendo matching de los puntos captados en SHARKS y VIKING
+
+dat1=Table.read("Sharks_sgp_e_2_cat_small.fits", format="fits")
+df1=dat1.to_pandas()
+
+dat2=Table.read("VIKING_Ks.fit", format="fits")   
+df2=dat2.to_pandas()
+
+dec1 = df1["DELTA_J2000"]
+ra1 = df1["ALPHA_J2000"]
+mag1 = df1["MAG_AUTO"]
+
+dec2 = df2["DEJ2000"]
+ra2 = df2["RAJ2000"]
+mag2 = df2["Ksapc6"]   #Kspmag es petrosian magnitude. También: Ksap3, Ksap4, Ksap6 (fuentes puntuales) y Ksapc3, Ksapc4, Ksap6...
+
+rad=0.000277778   #Un segundo sexagesimal (segundo de arco en grados, las unidades en que se expresan ra1...)
+nside=4096 # healpix nside
+maxmatch=1 # return closest match 
+
+#Uso la función match para hacer el matching de los catálogos, llamando a las funciones antes definidas. 
+
+matches_viking = smatch.match(ra1, dec1, rad, ra2, dec2, nside=nside, maxmatch=maxmatch)  #Matching SHARKS-VIKING
+
+MAG_SHARKS  = mag1[matches_viking['i1']]
+MAG_VIKING = mag2[matches_viking['i2']]
+
+
+print(MAG_SHARKS)
+
+print(MAG_VIKING)
+
+dif=[]
+for r1,r2 in zip(MAG_SHARKS,MAG_VIKING):
+    #print(r1,r2)
+    dif.append(np.abs(r2-r1))  #multi por 3600 para pasar a grados
+    
+seaborn.distplot(dif)
+#plt.plot(MAG_SHARKS,dif)
