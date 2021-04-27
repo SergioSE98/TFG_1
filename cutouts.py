c144a57805a2ba73da1d3829722f22095eda201e
SergioSE98 <75576459+SergioSE98@users.noreply.github.com>
2021-04-27 10:44:32 +0100

    Programa cut-outs
    Subido programa cut-outs

---
+++ cutouts.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 26 12:01:45 2021
+
+@author: Sergi
+"""
+
+from astrocut import fits_cut
+
+from astropy.io import fits
+
+from astropy.coordinates import SkyCoord
+
+input_files = [r"C:\Users\Sergi\Desktop\Universidad\TFG\imagenes\sharks_er1_00h36-030d20_mosaic_ks_deepimage_1000000074480.fits"]
+#Aquí vendra una lista con las imágenes de DES en r,i,z y SHARKS en Ks, por ahora puedes probar solo con la imagen de SHARKS
+
+
+
+center_coord = SkyCoord("9.213583 -30.32642", unit='deg')  #Aquí defino la coordenada del objeto, por ejemplo, en este caso, RA=9.213583 y Dec=-30.32642
+
+cutout_size = [100,100]  #Tamaño de la imagen en píxeles, en este caso, creo que 100x100 es suficiente, pero podrás querer jugar con estos valores
+
+cutout_file = fits_cut(input_files, center_coord, cutout_size,
+drop_after="", single_outfile=False)   #Esto te generará un archivo fits por cada imagen en input_files.
+
+
+#A continuación, abrirás el fits file creado en ds9 y modificarás la scale adecuadamente para que se vea mejor, y guardarás la imagen en png o jpg.
